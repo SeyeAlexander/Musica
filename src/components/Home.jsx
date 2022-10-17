2532eab936592eee8877dc607d31e880cd7f553c
AceOluwapelumi <dagrandmaster01@gmail.com>
2022-10-17 01:47:01 +0100

    fixed inconsistent margins for the musicList and the player components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,18 @@
-import { Box, Container } from "@chakra-ui/react";
-import HeroSection from "./HeroSection";
-import NavBar from "./NavBar";
-import MusicList from "./MusicList";
-import Player from "./Player";
-import Audio from "./Audio";
+import { Box, Container } from '@chakra-ui/react';
+import HeroSection from './HeroSection';
+import NavBar from './NavBar';
+import MusicList from './MusicList';
+import Player from './Player';
 
 const Home = () => {
   return (
-    <Container maxW='8xl' mx='auto' position='relative'>
+    <Container maxW="8xl" mx="auto" position="relative">
       <NavBar />
       <HeroSection />
-      {/* <Audio /> */}
-      <MusicList banner={"Made for Seye"} />
-      <MusicList banner={"New releases"} />
-      <Box pb='100px'>
-        <MusicList banner={"Popular around you"} />
+      <MusicList banner={'Made for Seye'} />
+      <MusicList banner={'New releases'} />
+      <Box pb="150px">
+        <MusicList banner={'Popular around you'} />
       </Box>
       <Player />
     </Container>
--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -1,40 +1,36 @@
-import { Flex, VStack, Text, HStack, Box } from "@chakra-ui/react";
-import MusicItem from "./MusicItem";
-import one from "../assets/Rectangle 14.png";
-import two from "../assets/Rectangle 14 (1).png";
-import three from "../assets/Rectangle 14 (2).png";
-import four from "../assets/Rectangle 14 (3).png";
-import five from "../assets/Rectangle 14 (4).png";
-import six from "../assets/Rectangle 14 (5).png";
+import { Flex, VStack, Text, HStack, Box } from '@chakra-ui/react';
+import MusicItem from './MusicItem';
+import one from '../assets/Rectangle 14.png';
+import two from '../assets/Rectangle 14 (1).png';
+import three from '../assets/Rectangle 14 (2).png';
+import four from '../assets/Rectangle 14 (3).png';
+import five from '../assets/Rectangle 14 (4).png';
+import six from '../assets/Rectangle 14 (5).png';
 
 const MusicList = ({ banner, reference }) => {
   return (
-    <Box ml='6vw'>
-      <Box className='overflow' overflowY='hidden' w='100%' h='36vh'>
-        <Flex p='3'>
-          <VStack color='#EFEEE0' spacing={3} align='flexStart'>
-            <Text fontSize='lg' fontWeight='medium'>
+    <Box ml="90px">
+      <Box className="overflow" overflowY="hidden" w="100%" h="260px">
+        <Flex p="3">
+          <VStack color="#EFEEE0" spacing={3} align="flexStart">
+            <Text fontSize="lg" fontWeight="medium">
               {banner}
             </Text>
 
-            {/*When fetching data from an api or sourcing locally,keep list to 15 items
+            {/*When fetching data from an api or sourcing locally,keep list to 12 items
             because of HStack width, so the album arts don't shrink from prefered size */}
-            <HStack spacing='7' pb='5' w='200vw'>
-              <MusicItem url={one} title={"Life in a bubble"} />
-              <MusicItem url={two} title={"Mountain"} />
-              <MusicItem url={three} title={"Limits"} />
+            <HStack spacing="7" pb="5" w="2200px">
+              <MusicItem url={one} title={'Life in a bubble'} />
+              <MusicItem url={two} title={'Mountain'} />
+              <MusicItem url={three} title={'Limits'} />
               <MusicItem url={four} title={"Everything's black"} />
-              <MusicItem url={five} title={"Cancelled"} />
-              <MusicItem url={six} title={"Normad"} />
-              <MusicItem url={one} title={"Life in a bubble"} />
-              <MusicItem url={two} title={"Mountain"} />
-              <MusicItem url={three} title={"Limits"} />
+              <MusicItem url={five} title={'Cancelled'} />
+              <MusicItem url={six} title={'Normad'} />
+              <MusicItem url={one} title={'Life in a bubble'} />
+              <MusicItem url={two} title={'Mountain'} />
+              <MusicItem url={three} title={'Limits'} />
               <MusicItem url={four} title={"Everything's black"} />
-              <MusicItem url={five} title={"Cancelled"} />
-              <MusicItem url={six} title={"Normad"} />
-              <MusicItem url={one} title={"Life in a bubble"} />
-              <MusicItem url={two} title={"Mountain"} />
-              <MusicItem url={three} title={"Limits"} />
+              <MusicItem url={five} title={'Cancelled'} />
             </HStack>
           </VStack>
         </Flex>
